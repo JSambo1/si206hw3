689a18303bcb06effd77a9f6e4fadddde97e26b5
JSambo1 <73769184+JSambo1@users.noreply.github.com>
2022-09-23 18:13:29 -0400

    Add files via upload

--- HW3.py
+++ HW3.py
@@ -27,7 +27,7 @@ class Fortune_Teller():
     # For example : "Yes, No, Not clear"
 
     def __str__(self):
-        return str(", ".join(self.fortunes_list))
+        return  ", ".join([str(word) for word in self.fortunes_list])
 
     # create the get_fortune method
     # it randomly picks an index from 0 to the number of items in the fortunes_list minus one
@@ -35,13 +35,9 @@ class Fortune_Teller():
     # it returns the answer at the picked index
 
     def get_fortune(self):
-        length = len(self.fortunes_list)
-        randomInteger = random.randint(0, length-1)
-        self.fortunes_list.append(self.fortunes_list[randomInteger])
-        return self.fortunes_list[randomInteger]
-        # index = self.fortunes_list.randomint(0,-1)
-        # self.fortunes_list.append(index)
-        # return self.fortunes_list[index]
+        value = random.randint(0, (len(self.fortunes_list) -1))
+        self.fortunes_list.append(value)
+        return self.fortunes_list[value]
     
         
     # create the question_check method that takes a question
@@ -51,12 +47,12 @@ class Fortune_Teller():
     # returns the fortune from get_fortune
 
     def question_check(self, question):
-        for i in self.questions_list:
-            if question == self.questions_list[i]:
-                return "I've already answered that question"
-            else:
-                self.questions_list.append(question)
-        return self.get_fortune()
+        self.question = question
+        if self.question in self.questions_list:
+            return ("I've already answered that question")
+        else:
+             self.questions_list.append(self.question)
+             return self.get_fortune()
     # create the print_questions_history method
     # prints "[answer index] question - answer" for each of the indices in the fortunes_history_list
     # from the first to the last with each on a separate line.  If there are no items in the
@@ -66,8 +62,8 @@ class Fortune_Teller():
         if len(self.fortunes_history_list) == 0:
             print("None yet")
         else:  
-            for item in self.questions_list:
-                print("[" + str(self.questions_list.index(item)) + "]")
+            for item in self.fortunes_history_list:
+                print("[" + str(self.questions_list.index(item)) + "]" + self.questions_list[self.fortunes_history_list.indexx(item)]+ " - " + self.fortunes_list[self.fortunes_history_list.index(item)])
 
 
 
@@ -85,22 +81,23 @@ class Fortune_Teller():
     #   Not clear: 47
     #   The most frequent answer after 200 was Not clear
 
-    def main():
+def main():
 
     # You are welcome to replace the answer_list with your desired answers
         fortunes_list = ["Yes", "No", "Ask again", "Maybe", "Not clear"]
         bot = Fortune_Teller(fortunes_list)
 
     # get the first question or quit
-     
+        question = input("Enter a question or type quit: ")
     # loop while question is not "quit"
-        while i not in  self.questions_list == "quit":
-            question_check(bot)
+        while question != "quit":
+            
         # get an answer from question_check
-            print_questions_history(bot)
+            answer = bot.question_check(question)
         # print question - answer
-            question_check(bot)
+            print(question + " - " + answer)
         # get the next question or quit 
+            question = input("Enter a question or type quit:\n")
 
 def test():
 
@@ -146,5 +143,5 @@ def test():
 
 # only run the main function if this file is being run (not imported)
 if __name__ == "__main__":
-   main()
-   test() 
+    #main()
+    test() 
